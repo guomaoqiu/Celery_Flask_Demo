0fcae751bd07ce079afed01651f3b9bde2b00c27
guomaoqiu@sina.com <guomaoqiu@sina.com>
2018-04-16 18:03:52 +0800

    first commit

--- get.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#grep "https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us" tmp.txt | awk  '{print $2}' | awk -F '"' '{print $2}'
-
-grep "https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us" tmp.txt |grep -v "360p" | awk  '{print $2}' | awk -F '"' '{print $2}'  > tmp.log
